5a26246bfda2e8557cf7fbcf02109a3cc4242d41
Alexey Petrov <66418181+admin-nsk@users.noreply.github.com>
2021-04-07 17:11:24 +0700

    Change Pony ORM on Alchemy ORM

--- models.py
+++ models.py
@@ -1,6 +1,5 @@
 from decimal import Decimal
-
-from pony.orm import Database, Required, PrimaryKey, Optional, Set
+from sqlalchemy import create_engine, Table, Column, Float, Integer, String, MetaData, ForeignKey
 
 
 try:
@@ -9,48 +8,51 @@ except:
     exit("cannot open settings.py")
 
 
-db = Database()
+db = create_engine('sqlite:///Location.db:memory:', echo=True)
 db.bind(**DB_CONFIG)
 
-
-class Having_Houses(db.Entity):
-    """Table list"""
-    id = PrimaryKey(int, auto=True)
-    name = Required(str)
-    locations = Set('Location', reverse='having_houses')
+Base = declarative_base()
 
 
-class Location(db.Entity):
+class Location(Base):
     """Table squares"""
-    id = PrimaryKey(int, auto=True)
-    longitude = Required(Decimal)
-    latitude = Required(Decimal)
-    id_location = Required(int)
-    address = Optional(str)
-    area = Required(Decimal)
-    confluence_location = Set('Confluence')
-    having_houses = Optional(Having_Houses)
+    
+    __tablename__ = 'location'
+   id = Column(Integer, primary_key = True)
+   longitude = Column(Float, nullable=False)
+   latitude = Column(Float, nullable=False)
+   id_location = Column(Integer, nullable=False)
+   address = Column(String)
+   area = Column(Float, nullable=False)
+   having_houses = Column(ForeignKey('having_houses.id'))
 
 
-class Flats(db.Entity):
+class Flats(Base):
     """Table flats"""
-    id = PrimaryKey(int, auto=True)
-    longitude = Required(Decimal)
-    latitude = Required(Decimal)
-    floor = Required(str)
-    type_flat = Optional(str)
-    year_build = Optional(int)
-    material = Required(str)
-    type_house = Optional(str)
-    number_of_room = Required(int)
-    street = Required(str)
-    confluence_flat = Set('Confluence')
 
+   __tablename__ = 'flats'
+   id = Column(Integer, primary_key = True)
+   longitude = Column(Float, nullable=False)
+   latitude = Column(Float, nullable=False) 
+   floor = Column(String, nullable=False)
+   type_flat = Column(String)
+   year_build = Column(Integer)
+   material = Column(String, nullable=False)
+   type_house = Column(String)
+   number_of_room = Column(Integer, nullable=False)
+   street = Column(String, nullable=False)
+      
+class Having_Houses(Base):
+    """Table list"""
+    __tablename__ = 'having_houses'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable=False)
 
 class Confluence(db.Entity):
     """Table confluence building and location"""
-    id_flat = Required(Flats, reverse='confluence_flat')
-    id_location = Required(Location, reverse='confluence_location')
+    id_flat = Column(ForeignKey('flat.id'))
+    id_location = Column(ForeignKey('location.id'))
 
 
-db.generate_mapping(create_tables=True)
+Base.metadata.create_all(db)
